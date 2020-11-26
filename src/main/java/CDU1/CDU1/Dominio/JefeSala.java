7203558f561b4ffe062313968ba3f9ea67c77a52
PabloVelascoCrespo <Pablo.Velasco2@alu.uclm.es>
2020-11-26 12:38:28 +0100

    Version 0.1.0 Modulo1

--- src/main/java/CDU1/CDU1/Dominio/JefeSala.java
+++ src/main/java/CDU1/CDU1/Dominio/JefeSala.java
@@ -5,10 +5,6 @@ public class JefeSala extends Empleado{
 		public JefeSala(int iD, Restaurante restaurante, String nombre, String apellido, String telefono) {
 		super(iD, restaurante, nombre, apellido, telefono);
 		}
-		
-		public void ReservarMesa(Mesa m) {
-			m.setEstadoMesa(1);
-		}
 		public void AsignarCamareroMesa(Mesa m, CamareroMesa c) {
 			//If mesa est� libre???
 			m.setCamareroMesa(c);
