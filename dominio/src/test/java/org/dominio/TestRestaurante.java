fdc955c62631b04986ac0867f653e879c2fd838f
PabloVelascoCrespo <Pablo.Velasco2@alu.uclm.es>
2020-12-23 14:36:31 +0100

    Test

--- dominio/src/test/java/org/dominio/TestRestaurante.java
+++ dominio/src/test/java/org/dominio/TestRestaurante.java
@@ -13,14 +13,14 @@ public class TestRestaurante {
 	static Restaurante r;
 	static Empleado e1;
 	static Empleado e2;
-	static Empleado [] empleados;
-	
+	static Empleado[] empleados;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		e1 = new Empleado (1, null, "Fernando", "Torres", "654578756");
-		e2 = new Empleado (2, null, "Laura", "Torres", "658558556");
-		Empleado [] empleados = {e1, e2};
-		r = new Restaurante (0, "Mafia", "Ciudad Real", empleados, null);
+		e1 = new Empleado(1, null, "Fernando", "Torres", "654578756");
+		e2 = new Empleado(2, null, "Laura", "Torres", "658558556");
+		Empleado[] empleados = { e1, e2 };
+		r = new Restaurante(0, "Mafia", "Ciudad Real", empleados, null);
 	}
 
 	@AfterClass
@@ -33,6 +33,17 @@ public class TestRestaurante {
 
 	@After
 	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGetID() {
+		assertEquals(0, r.getID());
+	}
+
+	@Test
+	public final void testSetID() {
+		r.setID(5);
+		assertEquals(5, r.getID());
 	}
 
 	@Test
@@ -63,4 +74,47 @@ public class TestRestaurante {
 		assertEquals(empleados, r.getEmpleados());
 	}
 
+	@Test
+	public final void testSetTiempoTomaComandas() {
+		r.setTiempoTomaComandas(5);
+		assertEquals(5, r.getTiempoTomaComandas(), 0);
+	}
+
+	@Test
+	public final void testGetTiempoTomaComandas() {
+		assertEquals(0, r.getTiempoTomaComandas(), 0);
+	}
+
+	@Test
+	public final void testGetTiempoPrepComida() {
+		r.setTiempoPrepComida(5);
+		assertEquals(5, r.getTiempoPrepComida(), 0);
+	}
+
+	@Test
+	public final void testSetTiempoPrepComida() {
+		assertEquals(0, r.getTiempoPrepComida(), 0);
+	}
+
+	@Test
+	public final void testGetTiempoEntregaNota() {
+		r.setTiempoEntregaNota(5);
+		assertEquals(5, r.getTiempoEntregaNota(), 0);
+	}
+
+	@Test
+	public final void testSetTiempoEntregaNota() {
+		assertEquals(0, r.getTiempoEntregaNota(), 0);
+	}
+
+	@Test
+	public final void testGetTiempoMesaLire() {
+		r.setTiempoMesaLire(5);
+		assertEquals(5, r.getTiempoMesaLire(), 0);
+	}
+
+	@Test
+	public final void testSetTiempoMesaLire() {
+		assertEquals(0, r.getTiempoMesaLire(), 0);
+	}
 }
