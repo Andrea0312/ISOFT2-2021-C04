61ab03b2f35d944946bc4806087bea53ea81d4e5
PabloVelascoCrespo <Pablo.Velasco2@alu.uclm.es>
2020-12-23 13:06:45 +0100

    Test

---
+++ interfaz/src/test/java/org/interfaz/POVCocinaTest.java
@@ -0,0 +1,35 @@
+package org.interfaz;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class POVCocinaTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAvisarCamarero() {
+		assertEquals(false, Inicio.camareroAvisado);
+		Inicio.camareroAvisado = true;
+		assertEquals(true, Inicio.camareroAvisado);
+	}
+}
