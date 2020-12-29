f685f22bc125bd0cca86189594fc9e853e735647
MiguelPellicerDominguez <miguel.pellicer@alu.uclm.es>
2020-12-30 00:46:54 +0100

    Version 0.3.2

---
+++ interfaz/src/test/java/org/interfaz/TestCamAvisado.java
@@ -0,0 +1,74 @@
+package org.interfaz;
+
+import static org.junit.Assert.*;
+
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCamAvisado {
+
+	private final CamAvisado ca = new CamAvisado();
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link java.awt.Dialog#setVisible(boolean)}.
+	 */
+	@Test
+	public void testSetVisible() {
+		assertEquals(true,ca.isVisible());
+	}
+
+	@Test
+	public void testJLabel() {
+		for(Component c : ca.getContentPane().getComponents()) {
+			if (c instanceof JLabel) {
+				assertEquals("Los camareros han sido avisados satisfactoriamente",((JLabel) c).getText());
+			}
+		}
+	}
+
+	@Test
+	public void testJButton() {
+		for(Component c : ca.getContentPane().getComponents()) {
+			if (c instanceof JButton) {
+				assertEquals("OK",((JButton) c).getText());
+			}
+		}
+	}
+	
+}
